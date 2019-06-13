3b5ae46a302fa829378d572eceadd577c670b8f6
Nicki Nixon <nickitza@gmail.com>
2019-06-12 21:27:38 -0600

    form styles

--- client/src/components/DepartmentForm.js
+++ client/src/components/DepartmentForm.js
@@ -1,31 +1,93 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 class DepartmentForm extends React.Component {
+  state = {name: ""}
 
-  render(){
-    return(
+  handleChange = (e) => {
+    this.setState({ name: e.target.value, })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    const department = { ...this.state, };
+    axios.post("/api/departments", department)
+    .then( res => {
+      this.props.history.push("/departments");
+    })
+    this.setState({name: "" });
+  }
+
+  render() {
+    return (
       <>
-      <h1>New Department Form</h1>
-      <form>
-        <label for="name">
-        <span>Name of Department: </span>
-        <StyledInput
-        placeholder="Name"
-        >
-        </StyledInput>
-        </label>
-      </form>
+        <h1>New Department Form</h1>
+        <form onSubmit={this.handleSubmit}>
+          <label for="name">
+            <span>Name of Department: </span>
+            <StyledInput
+              placeholder="Name"
+              name="name"
+              value={this.state.name}
+              onChange={this.handleChange}
+              required
+            />
+          </label>
+          <label for="submit">
+            <StyledSubmit type="submit" />
+          </label>
+          <Link to="/departments">
+            <StyledBack>Back</StyledBack>
+          </Link>
+        </form>
       </>
-    )
+    );
   }
 }
+const StyledSubmit = styled.input`
+  padding: 1em;
+  background-color: #74a19d;
+  border-radius: 7px;
+  color: white;
+  text-transform: uppercase;
+  font-weight: 700;
+  margin-left: 5px;
+  transition: background 0.4s ease;
+  border: none;
 
-const StyledInput = styled.input `
+
+  &:hover {
+    background-color: #86BAB6;
+    transition: background 0.4s ease;
+
+  }
+`;
+
+
+const StyledBack = styled.button`
+  padding: 1em;
+  background-color: #bab2b5;
+  border-radius: 7px;
+  color: white;
+  text-transform: uppercase;
+  font-weight: 700;
+  margin-left: 5px;
+  border: none;
+
+  &:hover {
+    background-color: #878284;
+    transition: background 0.4s ease;
+
+  }
+`;
+
+const StyledInput = styled.input`
   padding: 1em;
   border-radius: 7px;
   width: 16%;
   margin-left: 20px;
-`
+`;
 
-export default DepartmentForm
+export default DepartmentForm;
--- client/src/components/Departments.js
+++ client/src/components/Departments.js
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import axios from "axios";
-import DepartmentForm from './DepartmentForm'
 import { Link } from 'react-router-dom'
 import styled from 'styled-components'
 
@@ -14,13 +13,25 @@ class Departments extends React.Component {
       })
   }
 
+  deleteDept = (id) => {
+    axios.delete(`/api/departments/${id}`)
+    .then( res => {
+        const { departments, } = this.state;
+        this.setState({ departments: departments.filter(d => d.id !== id), })
+      })
+  }
+
   renderDepartments = () => {
     const { departments } = this.state
 
     if (departments.length <= 0)
       return <h3>No Departments</h3>
     return departments.map( department => (
-      <h3>{ department.name }</h3>
+      <>
+      <h3>{ department.name }
+      <Delete onClick={this.deleteDept}>X</Delete>
+      </h3>
+      </>
     ))
 
   }
@@ -46,6 +57,26 @@ const StyledButton = styled.button`
   padding: 1em;
   border-radius: 10px;
   font-size: .8em;
+  font-weight: 700;
+  text-transform: uppercase;
+  border: none;
+  margin-left: 9%;
+  margin-top: 3%;
+  transition: background 0.4s ease;
+
+  &:hover{
+    background-color: #86BAB6;
+    transition: background 0.4s ease;
+  }
+`
+
+const Delete = styled.button`
+  background-color: #F7E74F;
+  border-radius: 100%;
+  border: none;
+  color: white;
+  margin-left: .5%;
+
 `
 
 export default Departments;
